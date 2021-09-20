5d90d2c19792c7dc96bc2572afc5cccbea030ea1
Diegol87 <da.infanta@gmail.com>
2021-09-20 13:16:12 -0300

    quinta version

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -12,5 +12,8 @@ $(function(){
             });
         }
     })
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)})
 });
 
